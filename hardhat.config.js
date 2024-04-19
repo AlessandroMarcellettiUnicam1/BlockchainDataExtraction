1c4b89a00bb177b66fb56e0a1a65045865a85b0b
Lorenzo <verduccii46@gmail.com>
2024-04-19 10:11:20 +0200

    feat: sepolia fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,10 +5,13 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/ISHV03DLlGo2K1-dqE6EnsyrP2GF44Gt",
-        blockNumber: 12427648,
-        chainId: 1
+        // url: "https://eth-mainnet.g.alchemy.com/v2/ISHV03DLlGo2K1-dqE6EnsyrP2GF44Gt",
+        url: "https://eth-sepolia.g.alchemy.com/v2/6mnvPmDfijyOL2xG05-M6lkoYZwU_d9X",
+        // blockNumber: 12427648,
+        blockNumber: 3895654,
       },
+      // chainId: 1
+      chainId: 11155111
     }
   },
   solidity: "0.8.19",
