3f5550b7c179fb8abcaca6f491f66d2af3456af3
nikoauso <niko.ausili@gmail.com>
2024-05-27 18:22:29 +0200

    Update query filtering system and add UI

--- query/query.js
+++ query/query.js
@@ -1,19 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
 const app = express();
-const saveTransaction = require("./saveTransaction");
 const Transaction = require("../schema/data");
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'query')));
-
+/*const saveTransaction = require("./saveTransactions")
 app.get('/save', saveTransaction);
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'query.html'));
-});
-
 app.get('/api/fields', (req, res) => {
     function getAllFields(schema, prefix = '') {
         let fields = [];
@@ -37,28 +27,36 @@ app.post('/api/all', (req, res) => {
     Transaction.find()
         .then(transactions => res.json(transactions))
         .catch(err => res.status(500).json({error: err.message}));
-});
+});*/
 
 app.post('/api/query', (req, res) => {
-    const query = req.body;
-    //console.log(query);
+    const {gasUsedFrom, gasUsedTo, blockNumberFrom, blockNumberTo, timestampFrom, timestampTo, ...rest} = req.body;
+
+    const query = {...rest};
+
+    if (gasUsedFrom || gasUsedTo) {
+        query.gasUsed = {};
+        if (gasUsedFrom) query.gasUsed.$gte = gasUsedFrom;
+        if (gasUsedTo) query.gasUsed.$lte = gasUsedTo;
+    }
+
+    if (blockNumberFrom || blockNumberTo) {
+        query.blockNumber = {};
+        if (blockNumberFrom) query.blockNumber.$gte = blockNumberFrom;
+        if (blockNumberTo) query.blockNumber.$lte = blockNumberTo;
+    }
+
+    if (timestampFrom || timestampTo) {
+        query.timestamp = {};
+        if (timestampFrom) query.timestamp.$gte = new Date(timestampFrom);
+        if (timestampTo) query.timestamp.$lte = new Date(timestampTo);
+    }
+
+    console.log(query)
+
     Transaction.find(query)
         .then(transactions => res.json(transactions))
         .catch(err => res.status(500).json({error: err.message}));
-});
-
-app.post('/api/find', (req, res) => {
-    const id = req.body.id;
-    //console.log(id);
-
-    Transaction.findById(id)
-        .then(transaction => {
-            if (!transaction) {
-                return res.status(404).json({ message: 'Transazione non trovata' });
-            }
-            res.json(transaction);
-        })
-        .catch(err => res.status(400).json({ message: err.message }));
 });
 
 module.exports = app;
