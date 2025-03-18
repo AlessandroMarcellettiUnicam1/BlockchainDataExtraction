7d0ca58c661c6eade29b972a50dc00d514891f76
Lorenzo Verducci <verduccii46@gmail.com>
2025-03-18 12:09:01 +0100

    fix: bug with uploading

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 // Middleware: Serving static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.json({limit: '10mb'}));
 
 const {searchTransaction} = require('./query/query');
 const {connectDB} = require("./config/db");
@@ -31,7 +31,6 @@ const { setObjectTypes } = require('./ocelMapping/objectTypes/objectTypes');
 
 app.post('/api/ocelMap',(req,res)=>{
     const ocelMap=req.body;
-    console.log(req.body);
     let ocel = {
         eventTypes: [],
         objectTypes: [],
