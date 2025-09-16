1f2ae040765f7f533ad4e885285b18ac76c1777e
LolloLauz <lorenzo.luzi@cooder.it>
2025-09-16 09:46:27 +0200

    Size of file handled increased

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 // Middleware: Serving static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "100mb" }));
 
 const { searchTransaction, queryData } = require("./query/query");
 const { connectDB } = require("./config/db");
