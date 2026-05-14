e7dc534bd9591402b5645dbcfbd52c8a526a4bc6
Giulio Parrucci <giuparrucc@gmail.com>
2026-05-14 14:20:47 +0200

    added bullmq queue config for mempool filtered fetch

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,9 +1,17 @@
 const Redis = require('ioredis');
+const{ Queue } = require('bullmq');
 require('dotenv').config();
 
-const redisClient = new Redis({
+const connectionOptions = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || 6379,
 });
 
-module.exports = redisClient;
+const redisClient = new Redis(connectionOptions);
+const txQueue = new Queue('mempool-queue', { connection: connectionOptions });
+
+module.exports = {
+    redisClient,
+    txQueue,
+    connectionOptions 
+}
